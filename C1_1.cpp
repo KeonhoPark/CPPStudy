00ae7777872312fea1e300d543f2564e54fa7050
KeonhoPark <gse04063@gmail.com>
2023-06-22 10:17:46 +0900

    입출력

---
+++ C1_1.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+
+int main(){
+	int num = 20;
+	std::cout<<"hello world!"<<std::endl;
+	std::cout<<"hello "<<"world!"<<std::endl;
+	std::cout<<num<<' '<<'A';
+	std::cout<<' '<<3.14<<std::endl;
+	return 0;
+}
