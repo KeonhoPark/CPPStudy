3d330b914d22395426f2a215ba6d8c211be28b18
KeonhoPark <gse04063@gmail.com>
2023-06-30 15:44:56 +0900

    ranged for

--- modernCpp/sort.cpp
+++ modernCpp/sort.cpp
@@ -12,7 +12,12 @@ int main(){
     int arr[5] = {3, 1, 5, 2, 4};
     sort(arr, arr + 5, myGreater());
 
-    for(int i = 0; i < 5; i++){
+    // for(int i = 0; i < 5; i++){
+    //     cout << arr[i] << endl;
+    // }
+
+    //ranged for
+    for(int i:arr){
         cout << arr[i] << endl;
     }
 }
