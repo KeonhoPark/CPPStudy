b8206474f16f2024ca3475e625a8ac7a2171ba94
KeonhoPark <gse04063@gmail.com>
2023-06-30 12:52:54 +0900

    함수 객체

---
+++ functionObject.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+struct FuncObject{
+    void operator()(int n) const{
+        cout << "함수 객체 : " << n << endl;
+    }
+};
+
+void print(int n){
+    cout << "n : " << n << endl;
+}
+
+int main(){
+    // 함수 직접호출
+    print(10);
+    // 함수 포인터를 통한 호출
+    void (*fp)(int);
+    fp = print;
+    fp(1);
+    // 함수 객체 - Functor = 조건자
+    FuncObject fp2;
+    fp2(11);
+}
